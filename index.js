c2132d4c0ef1491c29f75427f11682ca3d33d1f6
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:15 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class instance extends instance_skel {
 	actions(system) {
 		var self = this;
 
-		self.system.emit('instance_actions', self.id, {
+		self.setActions({
 			'left':           { label: 'Pan Left',
 				options: [
 					{
